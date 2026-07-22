4177ddb894004927841d92fd8d555e695a159cf1
Mauricio-Siqueira <mauricio.rsiqueira009@gmail.com>
2026-07-22 16:54:14 -0300

    id, flag, is, is not e none

---
+++ Logica/Identidade.py
@@ -0,0 +1,24 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não valor 
+is e is not = é ou não é (tipo, valor , identidade)
+id = indentidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+v3 = 'b' 
+
+print(id(v1))
+print(id(v2))
+print(id(v3))
+
+# condição false
+
+
+# condicao = False
+
+# if condicao:
+#     print('faça algo')
+# else:
+#     print('Não faça algo')
