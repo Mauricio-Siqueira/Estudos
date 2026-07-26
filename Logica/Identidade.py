c711826b2b23b1640f7d7a3c044e32963fbc28be
Maurício-Siqueira <mauricio.rsiqueira009@gmail.com>
2026-07-26 13:02:56 -0300

    Atualização

--- Logica/Identidade.py
+++ Logica/Identidade.py
@@ -5,20 +5,27 @@ is e is not = é ou não é (tipo, valor , identidade)
 id = indentidade
 """
 
-v1 = 'a'
-v2 = 'a'
-v3 = 'b' 
+# v1 = 'a'
+# v2 = 'a'
+# v3 = 'b' 
 
-print(id(v1))
-print(id(v2))
-print(id(v3))
+# print(id(v1))
+# print(id(v2))
+# print(id(v3))
 
 # condição false
 
 
-# condicao = False
+condicao = False
+passou_no_if = None
 
-# if condicao:
-#     print('faça algo')
-# else:
-#     print('Não faça algo')
+if condicao:
+    passou_no_if = True
+    print('faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
