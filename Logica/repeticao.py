e77de14b2f1d523648e55c61761044c496ae74a5
Mauricio-Siqueira <mauricio.rsiqueira009@gmail.com>
2026-08-13 12:20:44 -0300

    exercicios

--- Logica/repeticao.py
+++ Logica/repeticao.py
@@ -68,3 +68,36 @@ while contador <= 100:
         break
 
 print("acabou")
+
+print(">>>>>>>>")
+
+# while dentro de While
+
+qtd_linha = 5
+qtd_coluna = 5
+
+linha = 1
+while linha <= 5:
+    coluna = 1
+    while coluna <= qtd_coluna:
+        print(f"{linha=} {coluna=}")
+        coluna += 1 
+
+    linha += 1
+
+
+print("acabou")
+
+
+# exercicio
+
+nome = "Mauricio"
+
+indice = 0
+novo_nome= ""
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f"*{letra}"
+    indice += 1
+
+print(novo_nome)
