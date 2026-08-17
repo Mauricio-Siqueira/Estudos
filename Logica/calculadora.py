afb18e48a54881c215dcefb10428af062273c289
Mauricio-Siqueira <mauricio.rsiqueira009@gmail.com>
2026-08-17 16:56:43 -0300

    calculo basico

--- Logica/calculadora.py
+++ Logica/calculadora.py
@@ -1,13 +1,15 @@
 while True:
-    numero_01 = input("Digite um número: ")
-    numero_02 = input("Digite outro número: ")
+    numero_1 = input("Digite um número: ")
+    numero_2 = input("Digite outro número: ")
     operador = input("Digite um operador(+, -, *, /): ")
 
     numero_valido = None
+    num_1_float = 0
+    num_2_float = 0
 
     try:
-        nun_1_ = float(numero_01)
-        nun_2_ = float(numero_02)
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
         numero_valido = True
     except:
         numero_valido = None
@@ -17,13 +19,31 @@ while True:
         continue
 
     operadores_permitidos = "+-/*"
+
     if operador not in operadores_permitidos:
-        print("Operador invalido")
+        print("Digite apenas um operador")
         continue
 
     if len(operador) > 1:
         print("Digite apenas um operador")
         continue
+
+    print("Realizado sua conta. Consfira o resultado")
+
+    if operador == "+":
+        print(num_1_float + num_2_float)
+
+    elif operador == "-":
+        print(num_1_float - num_2_float)
+
+    elif operador == "/":
+       print(num_1_float / num_2_float)
+
+    elif operador == "*":
+       print(num_1_float * num_2_float)
+    else:
+        print("Não era ára ter chegado aqui")    
+    
 
     sair = input("Quer sair? Digite [s]air: ").lower().startswith("s")
 
