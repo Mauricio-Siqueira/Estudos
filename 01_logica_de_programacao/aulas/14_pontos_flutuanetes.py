ed2045051c24d63151022016e0fbb79fc0619006
Mauricio-Siqueira <mauricio.rsiqueira009@gmail.com>
2026-09-22 14:36:45 -0300

    ponto flutuante

---
+++ 01_logica_de_programacao/aulas/14_pontos_flutuanetes.py
@@ -0,0 +1,14 @@
+'''
+Imprecisão dos pontos flutuantes
+Double-precision flating-ponit format IEEE 754
+'''
+import decimal
+
+numero_01 = decimal.Decimal(0.7)
+# numero_01 = 0.7
+numero_02 = decimal.Decimal(0.1)
+# numero_02 = 0.1
+numero_03 = numero_01 + numero_02
+print(numero_03)
+print(f'{numero_03:.2f}')
+print(round(numero_03, 2))
